20144f658b8f2c827b2deec22132fa2fbe90c00b
Pooja Das <poojadas@Poojas-MacBook-Pro.local>
2021-10-03 18:43:57 +0530

    Added code to Add item to cart, updated the shop data, added quantity of items added

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,13 @@
+export const addItemToCart = (cartItems, itemToAdd) => {
+  const existingCardItem = cartItems.find(
+    cartItem => cartItem.id === itemToAdd.id
+  );
+  if (existingCardItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === itemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...itemToAdd, quantity: 1 }];
+};
